63ced6f9aaf41d63cc325f775501aa73f35fc5ea
Peter Mwansa <mwansapeter800@gmail.com>
2022-12-29 02:40:04 +0300

    Add animation to all the text

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 window.addEventListener('scroll', () =>{
-    let content = document.querySelector('.row');
+    let content = document.querySelector('.row-happy');
     let contentPosition = content.getBoundingClientRect().top;
     let screenPosition = window.innerHeight;
 
     if(contentPosition < screenPosition) {
-        content.classList.add('active');
+        content.classList.add('active-happy');
     } else {
-        content.classList.remove('active');
+        content.classList.remove('active-happy');
+    }
+});
+
+window.addEventListener('scroll', () =>{
+    let content = document.querySelector('.row-wish');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+
+    if(contentPosition < screenPosition) {
+        content.classList.add('active-wish');
+    } else {
+        content.classList.remove('active-wish');
+    }
+});
+
+window.addEventListener('scroll', () =>{
+    let content = document.querySelector('.row-vision');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+
+    if(contentPosition < screenPosition) {
+        content.classList.add('active-vision');
+    } else {
+        content.classList.remove('active-vision');
+    }
+});
+
+window.addEventListener('scroll', () =>{
+    let content = document.querySelector('.row-thanks');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+
+    if(contentPosition < screenPosition) {
+        content.classList.add('active-thanks');
+    } else {
+        content.classList.remove('active-thanks');
+    }
+});
+
+window.addEventListener('scroll', () =>{
+    let content = document.querySelector('.row-clients');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+
+    if(contentPosition < screenPosition) {
+        content.classList.add('active-clients');
+    } else {
+        content.classList.remove('active-clients');
     }
 });
